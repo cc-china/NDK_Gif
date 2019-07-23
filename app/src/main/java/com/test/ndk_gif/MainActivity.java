b07c7fa0965b913ba60cd421685c65168ca0f3fd
lolo980 <com_caoyun@163.com>
2019-07-23 10:39:29 +0800

    NDK开发demo

---
+++ app/src/main/java/com/test/ndk_gif/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.ndk_gif;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2019\7\8 0008.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
